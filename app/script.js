62e30da06864cbe32b2150cafe934d0c316803ca
attano001 <attano001@outlook.com>
2022-02-19 11:32:17 +0100

    Finalizing the design

--- app/script.js
+++ app/script.js
@@ -1,1 +1,19 @@
-document.querySelector('clients-imgs').setAttribute('draggable', false); // to prevent user from dragging the logos in the clients section
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
